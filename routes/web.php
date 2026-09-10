b88fd8db13a7993d84cacf0ff15bed23e5428ba7
unknown <dimas.makassar.2020@gmail.com>
2026-09-10 11:18:13 +0800

    add health check route

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,10 @@ use App\Http\Controllers\LandingController;
 
 Route::get('/', [LandingController::class, 'index']);
 
+Route::get('/health', function () {
+    return 'SkillMate Laravel is running!';
+});
+
 /*
 |--------------------------------------------------------------------------
 | Dashboard User
